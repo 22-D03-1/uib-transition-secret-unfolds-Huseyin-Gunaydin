1b411a16eed55d67078473eb4817362e78512a55
= <=>
2022-06-01 13:32:40 +0200

    letzte commit

---
+++ main.js
@@ -0,0 +1,31 @@
+function türkisch() {
+  var yazdir = document.querySelector("h1");
+  yazdir.innerHTML = "Merhaba";
+ 
+}
+
+function deutsch() {
+    var yazdir = document.querySelector("h1");
+
+    yazdir.innerHTML="Hallo";
+}
+
+  function englisch() {
+    var yazdir = document.querySelector("h1");
+
+      yazdir.innerHTML="Hello";
+  }
+
+  function französich() {
+    var yazdir = document.querySelector("h1");
+
+      yazdir.innerHTML="Salut";
+  }
+
+
+
+  const el=() =>{
+
+
+
+  }
